7542baaca7be746ce5c031a783e99fd713c4906d
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2023-10-16 11:18:16 +0530

    code for a unkown website

---
+++ script.js
@@ -0,0 +1,4 @@
+// Sample animation when the "Hire Me" button is clicked.
+document.querySelector('.hire-btn').addEventListener('click', function() {
+    alert('Thank you for showing interest! We will get back to you shortly.');
+});
